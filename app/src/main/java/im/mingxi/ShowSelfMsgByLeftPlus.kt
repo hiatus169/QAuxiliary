a5d611116723ff9d88127c39867ac4ae2760d7f0
明熙 <158139013+hiatus169@users.noreply.github.com>
2025-04-13 22:31:39 +0800

    Add files via upload

---
+++ app/src/main/java/im/mingxi/ShowSelfMsgByLeftPlus.kt
@@ -0,0 +1,30 @@
+package im.mingxi
+
+import io.github.qauxv.base.annotation.FunctionHookEntry
+import io.github.qauxv.base.annotation.UiItemAgentEntry
+import io.github.qauxv.dsl.FunctionEntryRouter
+import io.github.qauxv.hook.CommonSwitchFunctionHook
+import io.github.qauxv.util.Initiator
+import io.github.qauxv.util.xpcompat.XC_MethodHook
+import io.github.qauxv.util.xpcompat.XposedBridge
+
+@FunctionHookEntry
+@UiItemAgentEntry
+object ShowSelfMsgByLeft : CommonSwitchFunctionHook() {
+
+    override val name = "自己消息居左 Plus"
+
+    override val description = "将自己的消息滑至左边"
+
+    override val uiItemLocation: Array<String> = FunctionEntryRouter.Locations.Auxiliary.CHAT_CATEGORY
+
+    override fun initOnce(): Boolean {
+        val chatItemClass = io.github.qauxv.util.Initiator.loadClass("com.tencent.mobileqq.activity.aio.ChatItemAnimLayout")
+        XposedBridge.hookMethod(chatItemClass.declaredMethod("setFrom", Boolean::class.java), object: XC_MethodHook() {
+            override fun beforeHookedMethod(param: MethodHookParam) {
+                param.args[0] = false
+            }
+        })
+        return true
+    }
+}
